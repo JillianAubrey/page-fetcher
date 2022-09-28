a4aca5a438052f7c73a31a3d47513e35b12a7008
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 19:46:07 +0000

    Matched console logs to expected values, completing exercise

--- fetcher.js
+++ fetcher.js
@@ -5,14 +5,14 @@ const path = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
+    console.log('statusCode:', response && response.statusCode);
     return console.log('connection error:', error);
   }
-  console.log('statusCode:', response && response.statusCode);
   // console.log('body:', body);
   fs.writeFile(path, body, error => {
-      if (error) {
-        return console.log('file error:', error);
-      }
-      console.log('File created');
-    })
+    if (error) {
+      return console.log('file error:', error);
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+  });
 });
