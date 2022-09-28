be2d012199181bec14b1034f9277d07fc8827b7c
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 20:13:55 +0000

    When file exists, user is asked whether they would like to overwrite it

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,23 @@
 const request = require('request');
+const readline = require('readline');
 const fs = require('fs');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const url = process.argv[2];
 const path = process.argv[3];
+
 
 const writeFile = function(path, text) {
   fs.writeFile(path, text, error => {
     if (error) {
       return console.log('file error:', error);
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    console.log(`Downloaded and saved ${text.length} bytes to ${path}`);
   });
-}
+};
 
 request(url, (error, response, body) => {
   if (error) {
@@ -19,12 +26,21 @@ request(url, (error, response, body) => {
   if (response.statusCode !== 200) {
     return console.log(response.statusCode, response.statusMessage);
   }
-  // console.log('body:', body);
-  fs.access(path, fs.constants.F_OK, (error) => {
-    if (!error) { //if there's no error, the file exists
-      return console.log(`${path} already exists!`);
-    }
-    writeFile(path, body);
-  })
 
+  fs.access(path, fs.constants.F_OK, (fileDoesntExist) => {
+    if (fileDoesntExist) {
+      rl.close();
+      return writeFile(path, body);
+    }
+    console.log(`${path} already exists!`);
+    rl.question('Would you like to overwrite it? (Y/N) ', answer => {
+      if (answer.toLowerCase() !== 'y') {
+        console.log(`${path} was not overwritten.`);
+        rl.close();
+        return;
+      }
+      writeFile(path, body);
+      rl.close();
+    });
+  });
 });
