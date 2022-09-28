b8ceccb787996974a491fe6eb0e3ca3f25ea4c78
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 19:42:45 +0000

    Fetcher.js is now writing the html to file

--- fetcher.js
+++ fetcher.js
@@ -5,8 +5,14 @@ const path = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
-    return console.log('error:', error);
+    return console.log('connection error:', error);
   }
   console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+  // console.log('body:', body);
+  fs.writeFile(path, body, error => {
+      if (error) {
+        return console.log('file error:', error);
+      }
+      console.log('File created');
+    })
 });
